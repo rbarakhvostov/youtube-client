affbf0a9aab3710a60193825644bd7a59c3013b4
Roman Barakhvostov <rbarakhvostov@gmail.com>
2019-05-18 21:12:10 +0300

    fix: change slider behavior

--- youtube-client/src/views/app-view/AppView.js
+++ youtube-client/src/views/app-view/AppView.js
@@ -1,7 +1,5 @@
 import './app-view.css';
 
-// window.pageCounter = 1;
-// window.loadingClipsPage_4 = 2;
 export default class AppView {
   constructor(clipInfo) {
     this.titles = clipInfo.titles;
@@ -21,28 +19,28 @@ export default class AppView {
     for (let i = 0; i < 15; i += 1) {
       document.querySelector('.clips-wrap').insertAdjacentHTML('beforeEnd',
         `<li class="clip"><div class="clip-title-wrap"><img class="clip-title-img" src="${this.pictures[i]}" alt="" width="320" height="180"><a class="title" href="https://www.youtube.com/watch?v=${this.clipId[i]}" target="_blank">${this.titles[i]}</a></div><div class="clip-info"><div class="clip-info-wrap"><div class="author-wrap"><span class="fas fa-user-alt"></span><span class="author">${this.authors[i].length <= 17 ? this.authors[i] : `${this.authors[i].slice(0, 14)}...`}</span></div><div class="date-wrap"><span class="fas fa-calendar-alt"></span><span class="date">${this.dates[i].slice(0, 10)}</span></div><div class="view-wrap"><span class="fas fa-eye"></span><span class="view">${this.viewCount[i]}</span></div></div><p class="description">${this.descriptions[i]}</p></div></li>`);
-      // document.querySelector('.clips-wrap').insertAdjacentHTML('beforeEnd',
-      //   `<div class="container"><li class="clip"><div class="clip-title-wrap"><img class="clip-title-img" src="${this.pictures[i]}" alt="" width="320" height="180"><a class="title" href="https://www.youtube.com/watch?v=${this.clipId[i]}" target="_blank">${this.titles[i]}</a></div><div class="clip-info"><div class="clip-info-wrap"><div class="author-wrap"><span class="fas fa-user-alt"></span><span class="author">${this.authors[i].length <= 20 ? this.authors[i] : `${this.authors[i].slice(0, 17)}...`}</span></div><div class="date-wrap"><span class="fas fa-calendar-alt"></span><span class="date">${this.dates[i].slice(0, 10)}</span></div><div class="view-wrap"><span class="fas fa-eye"></span><span class="view">${this.viewCount[i]}</span></div></div><p class="description">${this.descriptions[i]}</p></div></li></div>`);
     }
 
     document.addEventListener('mousemove', (e) => {
-      if (e.target.tagName !== 'INPUT') {
-        e.preventDefault();
-      }
+      e.preventDefault();
     });
+
+    // document.addEventListener('dblclick', (e) => {
+    //   e.preventDefault();
+    // });
 
     /* slider */
     const slider = document.querySelector('.clips-wrap');
+    const clip = document.querySelector('.clip:not(:first-child)');
+    const clipWidth = +(getComputedStyle(clip).width).slice(0, -2);
+    let clipMarginLeft = +(getComputedStyle(clip).marginLeft).slice(0, -2);
+    let htmlWidth = +(getComputedStyle(document.documentElement).width).slice(0, -2);
     let isDown = false;
     let startX;
     let scrollLeft;
-
     let x;
     let walk;
 
-    let offset;
-    // let counter = 1;
-    const { clientWidth } = document.documentElement;
     slider.addEventListener('mousedown', (e) => {
       isDown = true;
       slider.classList.add('active');
@@ -51,38 +49,57 @@ export default class AppView {
     });
 
     slider.addEventListener('mouseleave', () => {
-      isDown = false;
       slider.classList.remove('active');
+      if (isDown) {
+        if (walk <= -clipWidth) {
+          slider.scrollLeft = scrollLeft + htmlWidth + clipMarginLeft + 0.4;
+          window.pageCounter += 1;
+        }
+        if (walk > -clipWidth && walk < clipWidth && scrollLeft !== 0) {
+          slider.scrollLeft += walk + 0.6;
+        }
+        if (walk > -clipWidth && walk < clipWidth && scrollLeft === 0) slider.scrollLeft = 0;
+
+        if (walk >= clipWidth) {
+          slider.scrollLeft = scrollLeft - htmlWidth - clipMarginLeft;
+          window.pageCounter -= 1;
+          if (window.pageCounter < 1) window.pageCounter = 1;
+        }
+      }
+      isDown = false;
     });
 
     slider.addEventListener('mouseup', () => {
       isDown = false;
       slider.classList.remove('active');
-      if (walk <= -160) {
-        slider.scrollLeft = scrollLeft + clientWidth + offset + 0.4;
+      if (walk <= -clipWidth) {
+        slider.scrollLeft = scrollLeft + htmlWidth + clipMarginLeft + 0.4;
         window.pageCounter += 1;
-        // window.pageChange = true;
       }
-      if (walk > -160 && walk < 160 && scrollLeft !== 0) {
+      if (walk > -clipWidth && walk < clipWidth && scrollLeft !== 0) {
         slider.scrollLeft += walk + 0.6;
       }
-      if (walk > -160 && walk < 160 && scrollLeft === 0) slider.scrollLeft = 0;
+      if (walk > -clipWidth && walk < clipWidth && scrollLeft === 0) slider.scrollLeft = 0;
 
-      if (walk >= 160) {
-        slider.scrollLeft = scrollLeft - clientWidth - offset + 0.2;
+      if (walk >= clipWidth) {
+        slider.scrollLeft = scrollLeft - htmlWidth - clipMarginLeft;
         window.pageCounter -= 1;
-        // window.pageChange = false;
         if (window.pageCounter < 1) window.pageCounter = 1;
       }
-      // console.log(counter);
+      walk = 0;
     });
 
     slider.addEventListener('mousemove', (e) => {
       if (!isDown) return;
       x = e.pageX; // - slider.offsetLeft;
       walk = x - startX;
-      offset = +(getComputedStyle(slider.children[1]).marginLeft).slice(0, -2);
+      // offset = +(getComputedStyle(clip).marginLeft).slice(0, -2);
       slider.scrollLeft = scrollLeft - walk;
+    });
+
+    window.addEventListener('resize', () => {
+      htmlWidth = +(getComputedStyle(document.documentElement).width).slice(0, -2);
+      clipMarginLeft = +(getComputedStyle(clip).marginLeft).slice(0, -2);
     });
   }
 
