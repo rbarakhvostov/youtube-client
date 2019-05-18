be057cb09214f28187125405a347727e74ed38c1
Roman Barakhvostov <rbarakhvostov@gmail.com>
2019-05-18 22:37:56 +0300

    refactor: change structure of the project

--- youtube-client/src/views/app-view/AppView.js
+++ youtube-client/src/views/app-view/AppView.js
@@ -1,4 +1,5 @@
 import './app-view.css';
+import makeSlider from './slider';
 
 export default class AppView {
   constructor(clipInfo) {
@@ -21,86 +22,7 @@ export default class AppView {
         `<li class="clip"><div class="clip-title-wrap"><img class="clip-title-img" src="${this.pictures[i]}" alt="" width="320" height="180"><a class="title" href="https://www.youtube.com/watch?v=${this.clipId[i]}" target="_blank">${this.titles[i]}</a></div><div class="clip-info"><div class="clip-info-wrap"><div class="author-wrap"><span class="fas fa-user-alt"></span><span class="author">${this.authors[i].length <= 17 ? this.authors[i] : `${this.authors[i].slice(0, 14)}...`}</span></div><div class="date-wrap"><span class="fas fa-calendar-alt"></span><span class="date">${this.dates[i].slice(0, 10)}</span></div><div class="view-wrap"><span class="fas fa-eye"></span><span class="view">${this.viewCount[i]}</span></div></div><p class="description">${this.descriptions[i]}</p></div></li>`);
     }
 
-    document.addEventListener('mousemove', (e) => {
-      e.preventDefault();
-    });
-
-    // document.addEventListener('dblclick', (e) => {
-    //   e.preventDefault();
-    // });
-
-    /* slider */
-    const slider = document.querySelector('.clips-wrap');
-    const clip = document.querySelector('.clip:not(:first-child)');
-    const clipWidth = +(getComputedStyle(clip).width).slice(0, -2);
-    let clipMarginLeft = +(getComputedStyle(clip).marginLeft).slice(0, -2);
-    let htmlWidth = +(getComputedStyle(document.documentElement).width).slice(0, -2);
-    let isDown = false;
-    let startX;
-    let scrollLeft;
-    let x;
-    let walk;
-
-    slider.addEventListener('mousedown', (e) => {
-      isDown = true;
-      slider.classList.add('active');
-      startX = e.pageX; // - slider.offsetLeft;
-      ({ scrollLeft } = slider);
-    });
-
-    slider.addEventListener('mouseleave', () => {
-      slider.classList.remove('active');
-      if (isDown) {
-        if (walk <= -clipWidth) {
-          slider.scrollLeft = scrollLeft + htmlWidth + clipMarginLeft + 0.4;
-          window.pageCounter += 1;
-        }
-        if (walk > -clipWidth && walk < clipWidth && scrollLeft !== 0) {
-          slider.scrollLeft += walk + 0.6;
-        }
-        if (walk > -clipWidth && walk < clipWidth && scrollLeft === 0) slider.scrollLeft = 0;
-
-        if (walk >= clipWidth) {
-          slider.scrollLeft = scrollLeft - htmlWidth - clipMarginLeft;
-          window.pageCounter -= 1;
-          if (window.pageCounter < 1) window.pageCounter = 1;
-        }
-      }
-      isDown = false;
-    });
-
-    slider.addEventListener('mouseup', () => {
-      isDown = false;
-      slider.classList.remove('active');
-      if (walk <= -clipWidth) {
-        slider.scrollLeft = scrollLeft + htmlWidth + clipMarginLeft + 0.4;
-        window.pageCounter += 1;
-      }
-      if (walk > -clipWidth && walk < clipWidth && scrollLeft !== 0) {
-        slider.scrollLeft += walk + 0.6;
-      }
-      if (walk > -clipWidth && walk < clipWidth && scrollLeft === 0) slider.scrollLeft = 0;
-
-      if (walk >= clipWidth) {
-        slider.scrollLeft = scrollLeft - htmlWidth - clipMarginLeft;
-        window.pageCounter -= 1;
-        if (window.pageCounter < 1) window.pageCounter = 1;
-      }
-      walk = 0;
-    });
-
-    slider.addEventListener('mousemove', (e) => {
-      if (!isDown) return;
-      x = e.pageX; // - slider.offsetLeft;
-      walk = x - startX;
-      // offset = +(getComputedStyle(clip).marginLeft).slice(0, -2);
-      slider.scrollLeft = scrollLeft - walk;
-    });
-
-    window.addEventListener('resize', () => {
-      htmlWidth = +(getComputedStyle(document.documentElement).width).slice(0, -2);
-      clipMarginLeft = +(getComputedStyle(clip).marginLeft).slice(0, -2);
-    });
+    makeSlider();
   }
 
   renderNextPage() {
---
+++ youtube-client/src/views/app-view/slider.js
@@ -0,0 +1,78 @@
+export default function makeSlider() {
+  document.addEventListener('mousemove', (e) => {
+    e.preventDefault();
+  });
+
+  const slider = document.querySelector('.clips-wrap');
+  const clip = document.querySelector('.clip:not(:first-child)');
+  const clipWidth = +(getComputedStyle(clip).width).slice(0, -2);
+  let clipMarginLeft = +(getComputedStyle(clip).marginLeft).slice(0, -2);
+  let htmlWidth = +(getComputedStyle(document.documentElement).width).slice(0, -2);
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+  let x;
+  let walk;
+
+  slider.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX; // - slider.offsetLeft;
+    ({ scrollLeft } = slider);
+  });
+
+  slider.addEventListener('mouseleave', () => {
+    slider.classList.remove('active');
+    if (isDown) {
+      if (walk <= -clipWidth) {
+        slider.scrollLeft = scrollLeft + htmlWidth + clipMarginLeft + 0.4;
+        window.pageCounter += 1;
+      }
+      if (walk > -clipWidth && walk < clipWidth && scrollLeft !== 0) {
+        slider.scrollLeft += walk + 0.6;
+      }
+      if (walk > -clipWidth && walk < clipWidth && scrollLeft === 0) slider.scrollLeft = 0;
+
+      if (walk >= clipWidth) {
+        slider.scrollLeft = scrollLeft - htmlWidth - clipMarginLeft;
+        window.pageCounter -= 1;
+        if (window.pageCounter < 1) window.pageCounter = 1;
+      }
+    }
+    isDown = false;
+  });
+
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+    if (walk <= -clipWidth) {
+      slider.scrollLeft = scrollLeft + htmlWidth + clipMarginLeft + 0.4;
+      window.pageCounter += 1;
+    }
+    if (walk > -clipWidth && walk < clipWidth && scrollLeft !== 0) {
+      slider.scrollLeft += walk + 0.6;
+    }
+    if (walk > -clipWidth && walk < clipWidth && scrollLeft === 0) slider.scrollLeft = 0;
+
+    if (walk >= clipWidth) {
+      slider.scrollLeft = scrollLeft - htmlWidth - clipMarginLeft;
+      window.pageCounter -= 1;
+      if (window.pageCounter < 1) window.pageCounter = 1;
+    }
+    walk = 0;
+  });
+
+  slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    x = e.pageX; // - slider.offsetLeft;
+    walk = x - startX;
+    // offset = +(getComputedStyle(clip).marginLeft).slice(0, -2);
+    slider.scrollLeft = scrollLeft - walk;
+  });
+
+  window.addEventListener('resize', () => {
+    htmlWidth = +(getComputedStyle(document.documentElement).width).slice(0, -2);
+    clipMarginLeft = +(getComputedStyle(clip).marginLeft).slice(0, -2);
+  });
+}
